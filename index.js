25da592979bb8894d7d3c61c8ae9e408e7708083
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2024-04-13 11:39:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToDoList from './ToDoList.jsx';
+
+ReactDOM.createRoot(document.getElementById("root")).render(<ToDoList />);
